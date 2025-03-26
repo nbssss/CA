a5348ab3f1e6424beea740843104a188a955b5df
Natalia Borysowska-Ślęczka <natalia.borysowska@gmail.com>
2025-03-26 16:28:52 +0100

    v1-simple fire simulation

--- main.py
+++ main.py
@@ -1,69 +1,80 @@
-
 import numpy as np
 import random
 import time
 import os
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.colors import ListedColormap
 
-GRID_SIZE = 10
+GRID_SIZE = 100
 
 # cell states
-ALIVE = True
-DEAD = False
+TREE = 1    # healthy tree - could be fired
+FIRE = 2    # tree in fire
+ASH = 0     # burned tree
+WATER = 3
 
 # clear terminal window
 def clear_window():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def create_grid():
-    grid = np.zeros((GRID_SIZE, GRID_SIZE), dtype=bool)
+    grid = np.zeros((GRID_SIZE, GRID_SIZE))
 
     for i in range(GRID_SIZE):
         for j in range(GRID_SIZE):
-            temp = random.randint(0,4)
+            temp = random.random()
 
-            if temp == 0:
-                grid[i, j] = ALIVE
+            if temp < 0.5:
+                grid[i, j] = TREE
+            elif temp < 0.6:
+                grid[i,j] = WATER
             else:
-                grid[i, j] = DEAD
+                grid[i, j] = ASH
+
+    while True:
+        y, x = np.random.randint(0, GRID_SIZE, size=2)
+
+        if grid[y,x] == TREE:
+            grid[y,x] = FIRE
+            break
 
     return grid
 
 # [y,x] - grid position
-def count_alive_neighbours(y, x, grid):
+def count_neighbours_on_fire(y, x, grid):
     how_many = 0
 
     for i in range(y-1, y+2):
         for j in range(x-1, x+2):
-            if 0 <= i < GRID_SIZE and 0 <= j < GRID_SIZE and grid[i,j] == ALIVE:
+            if 0 <= i < GRID_SIZE and 0 <= j < GRID_SIZE and grid[i,j] == FIRE:
                 how_many += 1
 
-    # if [y,x] cell is alive decreasing how_much to exclude it from neighbours
-    if grid[y,x] == ALIVE:
+    # if [y,x] cell is on fire decreasing how_many to exclude it from neighbours
+    if grid[y,x] == FIRE:
         how_many -= 1
 
     return how_many
 
 
-
-# 3 neighbours = alive
-# more than 3 neighbours = dead
-# 2 or less neighbours = dead
-def evolution(grid):
+def evolution(frameNum, img, grid):
     how_many_neighbours = np.zeros((GRID_SIZE, GRID_SIZE))
 
     for i in range(GRID_SIZE):
         for j in range(GRID_SIZE):
-            how_many_neighbours[i,j] = count_alive_neighbours(i,j,grid)
+            how_many_neighbours[i,j] = count_neighbours_on_fire(i,j,grid)
 
     for i in range(GRID_SIZE):
         for j in range(GRID_SIZE):
-            if grid[i,j] == DEAD:
-                if how_many_neighbours[i,j] == 3:
-                    grid[i,j] = ALIVE
-            elif grid[i,j] == ALIVE:
-                if how_many_neighbours[i,j] < 2 or how_many_neighbours[i,j] > 3:
-                    grid[i,j] = DEAD
+            if grid[i,j] == FIRE:
+                grid[i,j] = ASH
+            elif grid[i,j] == TREE and how_many_neighbours[i,j] >= 1:
+                grid[i,j] = FIRE
 
+    img.set_data(grid)
+    return img
+
+'''
 def show_grid(grid):
     clear_window()
 
@@ -76,14 +87,20 @@ def show_grid(grid):
 
         print('\n', end='')
     print('\n', end='')
+'''
+
 
 def main():
     grid = create_grid()
 
-    while True:
-        show_grid(grid)
-        evolution(grid)
-        time.sleep(0.5)
+    fig, ax = plt.subplots()
+    fig.canvas.manager.set_window_title("Fire Simulator")
+
+    cmap = ListedColormap(["saddlebrown", "green", "red", "blue"])
+    img = ax.imshow(grid, cmap=cmap, vmin=0, vmax=3)
+    ani = animation.FuncAnimation(fig, evolution, fargs=(img,grid), frames=10, interval=150)
+
+    plt.show()
 
 
 if __name__ == '__main__':
